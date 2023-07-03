61249842c9fb690ce9c374270fad7b8806506b40
nikkirufiansya <nikkirufiansya88@gmail.com>
2023-07-03 15:42:06 +0700

    ok

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nikki/phalcon_sample',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nikki/phalcon_sample' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phalcon/devtools' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => 'c9bfb396123d2ad7deb90299ad5ae8d873341629',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phalcon/devtools',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
